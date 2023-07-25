dc4ba0da167ffc8a635c782a5b8350d801a27816
crossfire3001 <obiwan2035@mail.ru>
2023-07-25 10:44:13 +0500

    Navigation and working with data

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,9 +1,22 @@
 const getGoods = () => {
-  fetch("/db/db.json")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
+  const links = document.querySelectorAll(".navigation-link");
+
+  // получаем данные с json
+  const getData = () => {
+    fetch("/db/db.json")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  // получаем все ссылки и назначаем им клик
+  links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault(); // блокируем переход по url
+      getData(); // при каждом клике вызывается функция getData
     });
+  });
 };
 
 getGoods();
