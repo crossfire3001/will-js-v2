9dc925034a5067c2d3281fdf8ea29f9c6f3b87ba
crossfire3001 <obiwan2035@mail.ru>
2023-07-26 11:44:05 +0500

    localStorage: setting and deleting goods

--- js/getGoods.js
+++ js/getGoods.js
@@ -10,13 +10,22 @@ const getGoods = () => {
       });
   };
 
-  // получаем все ссылки и назначаем им клик
+  // получаем все ссылки и назначаем на них клик
   links.forEach((link) => {
     link.addEventListener("click", (e) => {
       e.preventDefault(); // блокируем переход по url
       getData(); // при каждом клике вызывается функция getData
     });
   });
+
+  localStorage.setItem("goods", JSON.stringify([1, 2, 3, 4, 5]));
+
+  const goods = JSON.parse(localStorage.getItem("goods"));
+  console.log(goods);
+
+  localStorage.removeItem("goods");
+
+  console.log(localStorage);
 };
 
 getGoods();
